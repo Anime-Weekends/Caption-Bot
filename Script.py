5a0bec6110994751b97be7be9d10fa3a5f47b601
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-03-14 23:11:47 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -5,24 +5,23 @@ class script(object):
     START_TXT = """<b>🍁 Hᴇʟʟᴏ {} !\n
 ɪ ᴀᴍ ᴛʜᴇ ᴍᴏꜱᴛ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ᴄᴀᴘᴛɪᴏɴ ʙᴏᴛ ᴡɪᴛʜ ᴘʀᴇᴍɪᴜᴍ ꜰᴇᴀᴛᴜʀᴇꜱ, ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ cʜᴀɴɴᴇʟ ᴀɴᴅ ᴇɴᴊᴏʏ
 
-‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/Silicon_Bot_Update'>Eᴍɪᴛɪɴɢ sᴛᴀʀs </a></b>
+‣ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : <a href='https://t.me/EmitingStars_Botz'>Eᴍɪᴛɪɴɢ sᴛᴀʀs </a></b>
 """
 
-    HELP_TXT = """•••[( 𝘎𝘦𝘵 𝘏𝘦𝘭𝘱 )]•••
+    HELP_TXT = """a
 
-❗ 𝗔𝗹𝗲𝗿𝘁 ❗
+<blockquote>❗ 𝗔𝗹𝗲𝗿𝘁 ❗</blockquote>
 
-• Aᴅᴅ ᴛʜɪs ʙᴏᴛ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ғᴜʟʟ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.
+<blockquote expandable>• Aᴅᴅ ᴛʜɪs ʙᴏᴛ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ғᴜʟʟ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.
 • Usᴇ ᴄᴏᴍᴍᴀɴᴅ ɢɪᴠᴇ ʙᴇʟᴏᴡ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ.
 • Tʜᴇsᴇ ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋ ɪɴ ᴄʜᴀɴɴᴇʟ.
 • Kᴇᴇᴘ ғɪʟᴇ ᴡɪᴛʜᴏᴜᴛ ғᴏʀᴡᴀʀᴅ ᴛᴀɢ.
 
 •> /set_cap - Sᴇᴛ Nᴇᴡ Cᴀᴘᴛɪᴏɴ Iɴ ʏᴏᴜʀ Cʜᴀɴɴᴇʟ
-•> /del_cap - Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴀᴘᴛɪᴏɴ
+•> /del_cap - Dᴇʟᴇᴛᴇ Yᴏᴜʀ Cᴀᴘᴛɪᴏɴ</blockquote>
 
-𝑭𝒐𝒓𝒎𝒂𝒕
-
-`{file_name}` = Oʀɪɢɪɴᴀʟ Fɪʟᴇ Nᴀᴍᴇ
+<blockquote expandable>𝗙𝗢𝗥𝗠𝗔𝗧</blockquote>
+<blockquote expandable>`{file_name}` = Oʀɪɢɪɴᴀʟ Fɪʟᴇ Nᴀᴍᴇ
 `{file_size}` = Oʀɪɢɪɴᴀʟ Fɪʟᴇ Sɪᴢᴇ 
 `{language}` = Lᴀɴɢᴜᴀɢᴇ Oғ Fɪʟᴇ Nᴀᴍᴇ
 `{year}` = Yᴇᴀʀ Oғ Fɪʟᴇ
@@ -31,22 +30,49 @@ class script(object):
 Eg:- `/set_cap
 {file_name}
 
-⚙️ Size » {file_size}
-
-╔═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
-💥 𝙅𝙊𝙄𝙉 :- ᴄʜᴀɴɴᴇʟ ʟɪɴᴋ 
-💥 𝙅𝙊𝙄𝙉 :- ᴄʜᴀɴɴᴇʟ ʟɪɴᴋ
-╚═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝`
+⚙️ Size » {file_size}</blockquote>
 """
 
-    ABOUT_TXT = """<b>╔════❰ ᴀᴜᴛᴏ ᴄᴀᴘᴛɪᴏɴ ʙᴏᴛ ❱═❍⊱❁
-║╭━━━━━━━━━━━━━━━➣
-║┣⪼📃ʙᴏᴛ : <a href='https://t.me/CustomCaptionBot'>Auto Caption</a>
-║┣⪼👦Cʀᴇᴀᴛᴏʀ : <a href='https://t.me/Silicon_Official'>Sɪʟɪᴄᴏɴ Dᴇᴠᴇʟᴏᴘᴇʀ ⚠️</a>
-║┣⪼🤖Uᴘᴅᴀᴛᴇ : <a href='https://t.me/Silicon_Bot_Update'>Sɪʟɪᴄᴏɴ Bᴏᴛᴢ™</a>
-║┣⪼📡Hᴏsᴛᴇᴅ ᴏɴ : ʜᴇʀᴏᴋᴜ 
-║┣⪼🗣️Lᴀɴɢᴜᴀɢᴇ : Pʏᴛʜᴏɴ3
-║┣⪼📚Lɪʙʀᴀʀʏ : Pʏʀᴏɢʀᴀᴍ 2.11.6
-║┣⪼🗒️Vᴇʀsɪᴏɴ : 2.0.8 [ᴍᴏsᴛ sᴛᴀʙʟᴇ]
-║╰━━━━━━━━━━━━━━━➣
-╚══════════════════❍⊱❁</b>"""
+    ABOUT_TXT = """<b><blockquote expandable>𝗔𝗕𝗢𝗨𝗧 𝗖𝗔𝗣𝗧𝗜𝗢𝗡 𝗙𝗢𝗡𝗧</blockquote>
+
+<blockquote expandable>➢ ʙᴏʟᴅ ᴛᴇxᴛ
+
+☞ <b>{filename}</b>
+
+➢ sᴘᴏɪʟᴇʀ ᴛᴇxᴛ
+
+☞ <spoiler>{filename}</spoiler>
+
+➢ ʙʟᴏᴄᴋǫᴜᴏᴛᴇ ᴛᴇxᴛ
+
+☞ <blockquote>{filename}</blockquote>
+
+➢ ʙʟᴏᴄᴋǫᴜᴏᴛᴇ ᴇxᴘᴀɴᴅᴇᴅ ᴛᴇxᴛ
+
+☞ <blockquote expandable>{filename}</blockquote>
+
+➢ ɪᴛᴀʟɪᴄ ᴛᴇxᴛ
+
+☞ <i>{filename}</i>
+
+➢ ᴜɴᴅᴇʀʟɪɴᴇ ᴛᴇxᴛ
+
+☞ <u>{filename}</u>
+
+➢ sᴛʀɪᴋᴇ ᴛᴇxᴛ
+
+☞ <s>{filename}</s>
+
+➢ ᴍᴏɴᴏ ᴛᴇxᴛ
+
+☞ <code>{filename}</code>
+
+➢ ʜʏᴘᴇʟɪɴᴋ ᴛᴇxᴛ
+
+☞ <a href="https://t.me/Anime_Weekends">{filename}</a>
+
+➢ ᴘʀᴇ ᴛᴇxᴛ
+
+☞ <pre>
+{file_name}
+</pre></blockquote></b>"""
