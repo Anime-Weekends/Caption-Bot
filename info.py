c91ed4b4bc9ad1f4d7e23d31cd85be46ed0a4107
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-05-17 15:45:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,7 @@
 from os import environ, getenv
 import re
 import os
+from typing import List
 
 id_pattern = re.compile(r"^.\d+$")
 
@@ -19,7 +20,8 @@ SILICON_PIC = os.environ.get("SILICON_PIC", "https://i.ibb.co/nshhg1Zp/photo-202
 API_ID = int(getenv("API_ID", "28744454"))
 API_HASH = str(getenv("API_HASH", "debd37cef0ad1a1ce45d0be8e8c3c5e7"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", "7733520743:AAG0HLnBXKQEqz9RC2v-MaSM88qi9UeVMJs"))
-FORCE_SUB = os.environ.get("FORCE_SUB", "EmitingStars_Botz") 
+IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "EmitingStars_Botz").split())) # Add Multiple channel id
 MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://NewData:NewData@cluster0.bctax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
 DEF_CAP = str(
     getenv(
